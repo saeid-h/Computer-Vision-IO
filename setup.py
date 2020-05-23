3797d5a93507bf1f8548c58ab4e2df69352bc46a
Saied <saied@ou.edu>
2020-05-22 21:55:16 -0400

    Fix bug: Program Language classification.

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setuptools.setup(
     url="https://github.com/saeid-h/Computer-Vision-IO.git",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2,3",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=2.7',
+    python_requires='>=3.6',
 )
 
 # python3 setup.py sdist bdist_wheel
